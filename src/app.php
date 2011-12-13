1a5d6371556e21b124d5b6d832bbcc2d33829bc9
Matt Robinson <matt@tui.co.uk>
2011-12-13 10:34:05 +0000

    Create initial base and index templates. Configure URL, Twig and Twig Bridge services in app.

--- src/app.php
+++ src/app.php
@@ -4,8 +4,27 @@ require __DIR__.'/../vendor/silex.phar';
 
 $app = new Silex\Application();
 
-$app->get('/', function() {
-    return 'Hello';
-});
+/* Services */
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path'       => __DIR__.'/../views',
+    'twig.class_path' => __DIR__.'/../vendor/twig/twig/lib',
+));
 
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\SymfonyBridgesServiceProvider(), array(
+    'symfony_bridges.class_path'  => __DIR__.'/../vendor/symfony/twig-bridge',
+));
+
+
+/* Routes and controllers */
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig', array(
+      'name' => 'world',
+    ));
+})
+->bind('homepage');
+
+
+/* Return configured app */
 return $app;
